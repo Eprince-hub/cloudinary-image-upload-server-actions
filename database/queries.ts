52b477f191d128961ce59b48b4837f27afd6cbb0
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-07-16 13:04:53 +0200

    Remove unused codes and files

--- database/queries.ts
+++ database/queries.ts
@@ -1,48 +1,6 @@
 import { cache } from 'react';
-import { User } from '../migrations/00000-createTableUsers';
 import { Image } from '../migrations/00001-createTableImages';
 import { sql } from './connect';
-
-export const getUsersInsecure = cache(async () => {
-  return await sql<User[]>`
-    SELECT
-      users.*
-    FROM
-      users
-  `;
-});
-
-export const createUserInsecure = cache(
-  async (
-    firstName: string,
-    lastName: string,
-    imageUrl: string,
-    uploadType: string,
-  ) => {
-    const [user] = await sql<User[]>`
-      INSERT INTO
-        users (
-          first_name,
-          last_name,
-          image_url,
-          type
-        )
-      VALUES
-        (
-          ${firstName},
-          ${lastName},
-          ${imageUrl},
-          ${uploadType}
-        )
-      RETURNING
-        users.first_name,
-        users.last_name,
-        users.image_url,
-        users.type
-    `;
-    return user;
-  },
-);
 
 export const getImagesInsecure = cache(async () => {
   return await sql<Image[]>`
