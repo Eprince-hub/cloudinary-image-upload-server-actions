5118a93485cf497cd0307b4bd305ecbe7a89c5f5
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-07-16 13:01:01 +0200

    Refactor page, remove user info

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,26 @@
 import Image from 'next/image';
-import { getImagesInsecure, getUsersInsecure } from '../database/queries';
+import { getImagesInsecure } from '../database/queries';
 import ImageForm from './images/ImageForm';
-import UserForm from './users/UserForm';
 
 export default async function Home() {
   const images = await getImagesInsecure();
-  const users = await getUsersInsecure();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold self-center">
-          Upload Image to Cloudinary
-        </h1>
-        <div className="grid grid-cols-2 gap-6">
-          {/* Display only images */}
+        <div className="p-4 max-w-[80%] mx-auto">
+          <h1 className="text-4xl font-bold self-center text-center">
+            Upload Image to Cloudinary
+          </h1>
+
           {images.length > 0 && (
             <div>
               <h2 className="text-2xl font-bold text-center">Images</h2>
-              <ul className="grid grid-cols-6 gap-4 mt-4">
+              <ul className="flex gap-4 flex-wrap my-4">
                 {images.map((image) => (
                   <li
                     key={`user-${image.id}`}
-                    className="flex items-center flex-col"
+                    className="flex items-center flex-col bg-teal-100 p-2 rounded-xl"
                   >
                     <Image
                       src={image.url}
@@ -40,40 +38,6 @@ export default async function Home() {
             </div>
           )}
           <ImageForm formTitle="Upload Image" buttonTitle="Upload Image" />
-        </div>
-
-        <div className="grid grid-cols-2 gap-6">
-          {/* Users profile info */}
-          {users.length > 0 && (
-            <div>
-              <h2 className="text-2xl font-bold text-center">Users</h2>
-              <ul className="grid grid-cols-6 gap-4 mt-4">
-                {users.map((user) => (
-                  <li
-                    key={`user-${user.id}`}
-                    className="flex items-center flex-col max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-                  >
-                    <Image
-                      src={user.imageUrl}
-                      alt="Uploaded image"
-                      className="w-7 h-7 rounded-full text-gray-500 dark:text-gray-400 mb-3"
-                      width={40}
-                      height={40}
-                    />
-                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                      {user.firstName}
-                    </h5>
-                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
-                      {user.lastName}
-                    </p>
-
-                    <span className="text-xs">{user.type}</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-          <UserForm buttonTitle="Create User" formTitle="Create User Profile" />
         </div>
       </main>
     </div>
