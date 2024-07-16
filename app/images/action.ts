a6fb14d69cc59611542dc6a31a8aa9e568fa67bd
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-07-16 13:01:20 +0200

    Refactor actions, call cloudinary in action

--- app/images/action.ts
+++ app/images/action.ts
@@ -1,35 +1,59 @@
 'use server';
 
+import { v2 as cloudinary } from 'cloudinary';
 import { createImageInsecure } from '../../database/queries';
-import { cloudinaryUpload } from '../../util/cloudinaryUpload';
+
+type CloudinaryResponse = {
+  secure_url: string;
+};
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export async function uploadImage(formData: FormData) {
   if (!formData.has('image')) {
-    return { error: 'No image selected' };
+    return { error: 'Please select an image' };
   }
 
   try {
-    const response = await cloudinaryUpload(formData, 'server-action-images');
+    const file = formData.get('image') as File;
 
-    if (!response.imageUrl) {
+    if (file.size > 1024 * 1024 * 5) {
+      return { error: 'Image is too large' };
+    }
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = new Uint8Array(arrayBuffer);
+
+    const response = await new Promise<CloudinaryResponse | undefined>(
+      (resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream({}, (error, result) => {
+            if (error) {
+              reject(error);
+              return;
+            }
+            resolve(result);
+          })
+          .end(buffer);
+      },
+    );
+
+    if (!response) {
       return { error: 'Image upload failed' };
     }
 
-    const image = await createImageInsecure(response.imageUrl, 'server action');
+    const image = await createImageInsecure(
+      response.secure_url,
+      'server action',
+    );
 
     if (!image) {
       return { error: 'Image upload failed' };
     }
 
-    // Line 15 to 19 above can be done in a single code block like this
-    // if (!(await createImageInsecure(response.imageUrl, 'server action'))) {
-    //   return { error: 'Image upload failed' };
-    // }
-
-    // revalidate the home page to display the new image that is returned from the database
-    // revalidatePath('/');
-
-    // You can also return the image and use router.refresh to revalidate from the client component
     return { image };
   } catch (uploadError) {
     return { error: (uploadError as Error).message };
